14a735bf55581bdfea71839d54975812bccd1a72
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2025-12-27 23:58:20 +0530

    Express Routing Completed

--- server.js
+++ server.js
@@ -10,27 +10,27 @@ app.get("/",(req,res)=>{
 
 
 // Define additional routes
-    // app.get('/about', (req, res) => {
-    //     res.send('This is the about page.');
-    // });
+    app.get('/about', (req, res) => {
+        res.send('This is the about page.');
+    });
 
 
 // Dynamic route with URL parameter
-    // app.get('/user/:username', (req, res) => {
-    //     const username=req.params.username;
-    //     res.send(`Welcome, ${username}!`);
-    // });
+    app.get('/user/:username', (req, res) => {
+        const username=req.params.username;
+        res.send(`Welcome, ${username}!`);
+    });
 
 // Route with query parameter
-    // app.get('/search', (req, res) => {
-    //     const keyword=req.query.keyword;
-    //     res.send(`You Search, ${keyword}!`);
-    // }); 
+    app.get('/search', (req, res) => {
+        const keyword=req.query.keyword;
+        res.send(`You Search, ${keyword}!`);
+    }); 
 
 // using Controllers (Module Route Handling)
     import { userLogin,userSignup } from './controller.js';
-    // app.get('/user/login', userLogin); 
-    // app.get('/user/signup', userSignup);
+    app.get('/user/login', userLogin); 
+    app.get('/user/signup', userSignup);
 
 // Using Express Router (Modular Route Handling)
     import router from './route.js';
